3e644a345c71d003fe9b82bde7fa0232a26db6da
rutupatel21 <rutupatel62@gmail.com>
2020-02-23 13:01:45 +0530

    adding candidate model and add candidate function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,15 +2,29 @@ pragma solidity ^0.5.16;
 
 contract Election {
 	
-	address public owner;
-	modifier restricted() {
-    	if (msg.sender == owner) _;
-  	}
-
 	string public candidate;
 	//constructor to store and read a candidate name
 	constructor () public{
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	//model candidate
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	//store candidate using mapping
+	mapping(uint => Candidate) public candidates;
+
+	//count candidates
+	uint public candidateCount;
+
+	function addCandidate (string memory _name) private {
+		candidateCount++;
+		candidates[candidateCount] = Candidate(candidateCount, _name, 0);
 	}
 
 }
