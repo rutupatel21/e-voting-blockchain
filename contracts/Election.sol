0e92d295a2a124ea3c8b2bd1ddc12edc65898fcd
rutupatel21 <rutupatel62@gmail.com>
2020-02-26 14:31:51 +0530

    adding UI files and voting function

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -18,13 +18,36 @@ contract Election {
 
 	//store candidate using mapping
 	mapping(uint => Candidate) public candidates;
+	//store accounts that have voted
+	mapping (address => bool) public voters;
+	
 
 	//count candidates
 	uint public candidateCount;
 
+	//voted event
+	event votedEvent(uint _candidateId);
+	
 	function addCandidate (string memory _name) private {
 		candidateCount++;
 		candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+	}
+
+	function vote (uint _candidateId) public {
+		// to check that the person has not voted before
+		require(!voters[msg.sender]);
+
+		// require a valid candidate
+		require(_candidateId > 0 && _candidateId <= candidateCount);
+		
+		//to record that voter has voted
+		voters[msg.sender] = true;
+		
+		//updating vote count
+		candidates[_candidateId].voteCount ++;
+
+		//trigger voted event
+		emit votedEvent(_candidateId);
 	}
 
 }
